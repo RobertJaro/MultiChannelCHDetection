1f6ed4f1e78310e65ccee2b48929db23db388329
RobertJaro <robert.jarolim@gmail.com>
2022-05-05 14:04:35 +0200

    update version description, set pip version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 from setuptools import setup
 
 setup(
-    name='MultiChannelCHDetection',
-    version='0.2',
+    name='chronnos',
+    version='1.0.0',
     packages=setuptools.find_packages(),
     url='https://github.com/RobertJaro/MultiChannelCHDetection',
     license='GPL-3.0',
     author='Robert Jarolim',
-    author_email='',
+    author_email='robert.jarolim@uni-graz.at',
     description='CHRONNOS',
     install_requires=['torch>=1.8', 'sunpy>=3.0', 'tqdm', 'drms', 'astropy', 'aiapy'],
 )
